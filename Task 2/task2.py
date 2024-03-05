5dcbe4415c207d6016fc9ee2b207d4906241a564
Angel0v22w <velislavangelov19@gmail.com>
2024-03-05 21:11:29 +0200

    Update - Task 2

--- Task 2/task2.py
+++ Task 2/task2.py
@@ -47,19 +47,21 @@ def update_info():
         weather, temp, humidity = get_weather_info(city)
         all_temp[city] = temp
         total_temperature += temp
-        tfield.insert(tk.INSERT,f"Град {city}\n Време {weather}\n Температура {temp:.1f}°C\n Влажност {humidity}\n --------------\n")
+        tfield.insert(tk.INSERT,f"Град {city}\n Време {weather}\n Температура {temp:.1f}°C\n Влажност {humidity}%\n --------------\n")
     
     min_temp = min(all_temp.values())
     average_temperature = total_temperature / len(random_cities)
     tfield.insert(tk.INSERT,f"Coldest city: {min_temp:.1f}°C ({find_city(min_temp,all_temp)})\n")
-    tfield.insert(tk.INSERT,f"Average temperature: {average_temperature:.1f}°C")
+    tfield.insert(tk.INSERT,f"Average temperature: {average_temperature:.1f}°C\n")
+    
 
 def info(city):
     weather, temp, humidity = get_weather_info(city)
-    tfield.insert(tk.INSERT,f"Град {city}\n Време {weather}\n Температура {temp}\n Влажност {humidity}\n")
+    tfield.insert(tk.INSERT,"\n")
+    tfield.insert(tk.INSERT,f"Град {city}\n Време {weather}\n Температура {temp}\n Влажност {humidity}%\n")
 
 window = tk.Tk()
-window.title("Прогноза за времето")
+window.title("Weather APP")
 
 main_frame = tk.Frame(window)
 search_frame = tk.Frame(window)
@@ -70,7 +72,7 @@ search_button = tk.Button(search_frame, text="Търсене", command=lambda: i
 
 main_frame.pack()
 search_frame.pack()
-tfield = tk.Text(main_frame, width=46, height=10)
+tfield = tk.Text(main_frame, width=46, height=30)
 tfield.pack()
 search_entry.pack(side=tk.LEFT)
 search_button.pack(side=tk.LEFT)
